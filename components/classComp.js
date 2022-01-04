b5dea9e193b1b6ee4b99dc5eb8fb9e1a3cc58234
Manojkumar <90634510+DeveloperMANOJKUMAR-LOTUS@users.noreply.github.com>
2022-01-04 21:23:50 +0530

    added the components

---
+++ components/classComp.js
@@ -0,0 +1,13 @@
+import {Component} from 'react';
+
+class ClassComp extends Component {
+    render(){
+        return(
+            <>
+              <h6>This is a Class Component .</h6>
+            </>
+        )
+    }
+}
+
+export default ClassComp ;
---
+++ components/functionalComp.js
@@ -0,0 +1,9 @@
+function FunctionalComp (){
+    return(
+        <div>
+            <h1 className="bg-primary text-white">This is a functional component .</h1>
+        </div>
+    );
+}
+
+export default FunctionalComp;
